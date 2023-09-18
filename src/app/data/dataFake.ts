01aeb5e3a70875256dd5707d720b017b958013e2
LuccPass <luccpass@gmail.com>
2023-09-18 20:14:06 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    'id':'1',
+    'title': 'NOVO HOMEM DE FERRO',
+    'description': 'MARVEL ANUNCIA UM NOVO FILME DO HOMEM DE FERRO, CONFIRA',
+    'photoCover': 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg'
+  },
+
+  {
+    'id':'2',
+    'title': 'Nova Serie anunciada no disney+',
+    'description': 'blabla',
+    'photoCover': 'https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg'
+  }
+]
